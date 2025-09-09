d0ba76bffea0b5be94c2c57e40fe4eb2dc049807
0laolu <abdullateefolaolu@gmail.com>
2025-09-09 23:18:06 +0100

    style: make hero section responsive

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
 
     return (
         <header>
-            <nav className="w-9/10 mx-auto pt-4 flex justify-between items-center">
+            <nav className="  w-9/10 mx-auto pt-4 flex justify-between items-center">
                 <div className="text-2xl font-bold">O.A.</div>
 
                 {/*hamburger menu*/}
@@ -32,12 +32,12 @@ export default function Navbar() {
                     <span className={`bg-black block w-full h-[3px] rounded-lg my-[5px] transition duration-400 lg:transition-none ${isActive ? "transform rotate-[45deg] translate-x-[-8px] translate-y-[-10px]" : "transform-none rotate-none translate-none"}`}></span>
                 </div>
 
-                <ul ref={navLinksRef} className={`bg-white invisible fixed w-full min-h-full top-0 left-0 transform translate-x-[-100%] transition duration-500 lg:visible lg:static lg:w-1/2 min-h-auto lg:transform-none lg:translate-x-[0] lg:transition-none ${isActive ? "visible transform-none translate-none": "invisible"}`}>
-                    <div className=" w-1/2 text-center mx-auto my-30 lg:text-left lg:mx-0 lg:my-0  lg:flex lg:justify-evenly">
-                         <li className="p-8">About</li>
-                        <li className="p-8">Projects</li>
-                        <li className="p-8">Contact</li>
-                        <li className="p-8">Resumé</li>
+                <ul ref={navLinksRef} className={` bg-white invisible fixed w-full min-h-full top-0 left-0 transform translate-x-[-100%] transition duration-500 lg:visible lg:static lg:w-1/2 min-h-auto lg:transform-none lg:translate-x-[0] lg:transition-none ${isActive ? "visible transform-none translate-none": "invisible"}`}>
+                    <div className=" w-1/2 text-center mx-auto my-30 lg:w-full lg:text-left lg:mx-0 lg:my-0  lg:flex lg:justify-evenly">
+                         <li className="p-8 cursor-pointer">About</li>
+                        <li className="p-8 cursor-pointer">Projects</li>
+                        <li className="p-8 cursor-pointer">Contact</li>
+                        <li className="p-8 cursor-pointer">Resumé</li>
                     </div>
                        
                     
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,28 +5,28 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 export default function Hero() {
     return(
         <section className=" md:w-9/10 mx-auto my-8">
-            <div className=" text-center xl:max-w-[1225px] mx-auto">
-                <h1 className="text-6xl/20 font-bold">Empowering Businesses with Fast, Scalable Web Solutions That Drive Growth.</h1>
-                <p className="my-8 font-league tracking-wide text-[1.2rem] max-w-[1130px] mx-auto">From dynamic restaurant menus to interactive real estate listings, e-commerce stores, and even startup landing pages — I build custom web applications that enhance user experience and boost conversions. Let's build a digital presence that propels your brand forward</p>
+            <div className="w-19/20 text-center lg:w-full xl:max-w-[1225px] mx-auto">
+                <h1 className="text-[2rem]/10 max-w-[640px] mx-auto lg:text-6xl/20 lg:max-w-none font-bold">Empowering Businesses with Fast, Scalable Web Solutions That Drive Growth.</h1>
+                <p className="my-8 text-[1.05rem] max-w-[712px] font-league tracking-normal lg:text-[1.6rem] lg:max-w-[1100px] mx-auto">From dynamic restaurant menus to interactive real estate listings, e-commerce stores, and even startup landing pages — I build custom web applications that enhance user experience and boost conversions. Let's build a digital presence that propels your brand forward!</p>
 
-                <div className="mt-8">
-                    <button className="w-1/5 border border-gray-400 py-4 rounded-4xl font-semibold cursor-pointer" type="button">Let's Build Your Website</button>
+                <div className="mt-12">
+                    <button className="w-4/5 max-w-[295px] border border-gray-400 py-4 rounded-xl font-semibold cursor-pointer lg:w-7/25 lg:max-w-none lg:text-xl" type="button">Let's Build Your Website</button>
                 </div>
 
-                <ul className="w-1/4 mx-auto mt-10 flex justify-evenly">
+                <ul className="w-4/5 max-w-[295px] mx-auto mt-16 flex justify-evenly lg:w-7/25 lg:max-w-none">
                     <li>
                         <a href="https://github.com/0laolu">
-                            <FontAwesomeIcon icon={faGithub} className="text-xl text-gray-800 p-3 border border-gray-400 rounded-full"/>            
+                            <FontAwesomeIcon icon={faGithub} className="text-xl text-gray-800 p-3 border border-gray-400 rounded-full lg:text-2xl"/>            
                         </a>
                     </li>
                     <li>
                         <a href="https://linkedin.com/in/olaoluwa-ajanaku">
-                            <FontAwesomeIcon icon={faLinkedin} className="text-xl text-gray-800 p-3 px-[0.82rem] border border-gray-400 rounded-full"/>
+                            <FontAwesomeIcon icon={faLinkedin} className="text-xl text-gray-800 p-3 px-[0.82rem] border border-gray-400 rounded-full lg:text-2xl"/>
                         </a>
                     </li>
                     <li>
                         <a href="mailto:abdullateefolaolu@gmail.com">
-                            <FontAwesomeIcon icon={faEnvelope} className="text-xl text-gray-800 p-3 border border-gray-400 rounded-full"/>
+                            <FontAwesomeIcon icon={faEnvelope} className="text-xl text-gray-800 p-3 border border-gray-400 rounded-full lg:text-2xl"/>
                         </a>
                     </li>
                     {/* <li>
